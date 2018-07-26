ae8833c355f1d6aef427586269b301ea5bfee29a
ledwonmat <mateusz.ledwon@softwaremill.com>
2018-07-26 08:52:36 +0200

    adds sjcl to build

--- src/lib/tests/crypto.test.ts
+++ src/lib/tests/crypto.test.ts
@@ -2,8 +2,40 @@ import { expect } from 'chai'
 
 import * as crypto from '../crypto'
 
-describe('encrypt', () => {
+describe('encrypt/decrypt', () => {
   it('should exist', () => {
     expect(crypto.encrypt).to.be.a('function')
+  })
+
+  it('should properly encrypt and decrypt message', () => {
+    const message = 'testMSG'
+    const password = 'abcd'
+    const encrypted = crypto.encrypt(password, message)
+    const decrypted = crypto.decrypt(password, encrypted)
+
+    expect(JSON.parse(encrypted)).to.have.ownProperty('cipher')
+    expect(decrypted).to.be.eq(message)
+  })
+
+  it('should produce sha512 hash', () => {
+    const message = 'testMSG'
+    const password = 'abcd'
+    const encrypted = crypto.encrypt(password, message)
+    const decrypted = crypto.decrypt(password, encrypted)
+
+    expect(JSON.parse(encrypted)).to.have.ownProperty('cipher')
+    expect(decrypted).to.be.eq(message)
+  })
+})
+
+describe('hashSha512', () => {
+  it('should exist', () => {
+    expect(crypto.hashSha512).to.be.a('function')
+  })
+
+  it('should produce sha512 hash', () => {
+    const result = crypto.hashSha512('testMessage')
+
+    expect(result).to.have.lengthOf(128)
   })
 })
