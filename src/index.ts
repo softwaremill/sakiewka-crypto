a878a9487456f19e1389c9311fa78271d8c3de48
ledwonmat <mateusz.ledwon@softwaremill.com>
2018-08-02 14:21:38 +0200

    export key module

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import * as address from './lib/address'
 import * as crypto from './lib/crypto'
 import * as transaction from './lib/transaction'
 import * as wallet from './lib/wallet'
+import * as key from './lib/key'
 
 export default {
   constants,
   address,
   crypto,
   transaction,
-  wallet
+  wallet,
+  key
 }
