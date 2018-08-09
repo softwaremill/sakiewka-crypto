29a5142e002896890b4ccf9e704e25dcb14b2e93
ledwonmat <mateusz.ledwon@softwaremill.com>
2018-08-09 15:42:31 +0200

    expose transactionEth

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as constants from './lib/constants'
 import * as address from './lib/address'
 import * as crypto from './lib/crypto'
 import * as transaction from './lib/transaction'
+import * as transactionEth from './lib/transaction-eth'
 import * as wallet from './lib/wallet'
 import * as key from './lib/key'
 import * as user from './lib/user'
@@ -11,6 +12,7 @@ export default {
   address,
   crypto,
   transaction,
+  transactionEth,
   wallet,
   key,
   user
