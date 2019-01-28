57cf98fb1f2123ae7242fecace198920bad26884
Piotr Buda <pibuda@gmail.com>
2019-01-28 13:16:23 +0100

    Adds base64 encoding of the resulting pdf

--- src/lib/pdfgen.ts
+++ src/lib/pdfgen.ts
@@ -1,15 +1,29 @@
 import PDFDocument from 'pdfkit'
+import {Base64Encode} from 'base64-stream'
 
 const QR_DATA_GAP = 15
 const QR_WIDTH = 128
 const QR_HEIGHT = 128
 
-export const generateBackupPdf = (): PDFKit.PDFDocument => {
+export const generateBackupPdfBase64 = (userPrivateKey: string, backupPrivateKey: string, servicePublicKey: string, encryptedPassword: string): Promise<string> => {
+  const doc = generateBackupPdf(userPrivateKey, backupPrivateKey, servicePublicKey, encryptedPassword)
+  const stream = doc.pipe(new Base64Encode())
+  var base64Pdf = ''
+  stream.on('data', (data:string) => base64Pdf += data)
+  return new Promise((resolve, reject) => {
+    stream.on('end', () => resolve(base64Pdf))
+    stream.on('error', () => reject())
+  })
+}
+
+const generateBackupPdf = (userPrivateKey: string, backupPrivateKey: string, servicePublicKey: string, encryptedPassword: string): PDFKit.PDFDocument => {
     const doc = new PDFDocument();
-    const userPrivateKey = '{"iv":"jPBIE/1S+NGckHMXVHDgcA==","v":1,"iter":10000,"ks":256,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"dV7sfCPyg3k=","ct":"2gJDm3OWenKfDfhVwvs3yLDk3IlwcDjUlnfdgCZ3bDs3Kv5h6BOgcEE5bLURwCQpdxJQbEj1ZdA0Ju2n7avJKJzf7OmdPPMt/P+UbQ8LlbX8tFV2Qdpj9C0rhX1JsgaG4Tyb5N+6znmRIdaVmlR8eATd0em6hIY="}'
-    const start = 150
+    const start = 130
+
+    printFlash(doc, 'Print this page or keep it securely offline!', 0, 0)
+
     drawDataBox(doc, {
-      header: 'A: User pkey',
+      header: 'A: User key',
       description: 'This is your password-encrypted private key',
       data: userPrivateKey,
       x: doc.page.margins.left,
@@ -19,28 +33,38 @@ export const generateBackupPdf = (): PDFKit.PDFDocument => {
     drawDataBox(doc, {
       header: 'B: Backup key',
       description: 'This is your password-encrypted backup private key',
-      data: userPrivateKey,
+      data: backupPrivateKey,
       x: doc.page.margins.left,
-      y: start * 2
+      y: start * 2 + 20
     })
 
     drawDataBox(doc, {
       header: 'C: Service public key',
       description: 'This is the public part of the key the service will use to co-sign transactions with you on Your wallet',
-      data: userPrivateKey,
+      data: servicePublicKey,
       x: doc.page.margins.left,
-      y: start * 3
+      y: start * 3 + 40
     })
 
     drawDataBox(doc, {
       header: 'D: Encrypted wallet password',
       description: 'This is your password-encrypted private key',
-      data: userPrivateKey,
+      data: encryptedPassword,
       x: doc.page.margins.left,
-      y: start * 4
+      y: start * 4 + 60
     })
+    
     doc.end()
     return doc
+}
+
+const printFlash = (doc: PDFKit.PDFDocument, message: string, x: number, y: number) => {
+  doc.save()
+  doc.rect(doc.page.margins.left, doc.page.margins.top, doc.page.width - doc.page.margins.right - doc.page.margins.left, 20)
+
+  doc.fillAndStroke("#faa", "#f55")
+  doc.restore()
+  doc.fontSize(10).fill("#000").text(message, doc.page.margins.left, doc.page.margins.top + 6, {align: 'center'}) // x has to be specified, otherwise align option is not used
 }
 
 const drawDataBox = (doc: PDFKit.PDFDocument, data: DataBoxConfig): PDFKit.PDFDocument => {
--- src/lib/tests/pdfgen.test.ts
+++ src/lib/tests/pdfgen.test.ts
@@ -1,9 +1,25 @@
-import {generateBackupPdf} from '../pdfgen'
+import {expect} from 'chai'
+import { generateBackupPdfBase64 } from '../pdfgen'
 import * as fs from 'fs'
 
-describe('test', () => {
-    it('should generate pdf file', async () => {
-        const doc = generateBackupPdf()
-        doc.pipe(fs.createWriteStream("/home/pb/Devel/sakiewka/test.pdf"))
+describe('PDF Backup Sheet generator', () => {
+    //This is a helper test to generate a file with the pdf
+    xit('should generate pdf file', async () => {
+        const userPrivateKey = '{"iv":"jPBIE/1S+NGckHMXVHDgcA==","v":1,"iter":10000,"ks":256,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"dV7sfCPyg3k=","ct":"2gJDm3OWenKfDfhVwvs3yLDk3IlwcDjUlnfdgCZ3bDs3Kv5h6BOgcEE5bLURwCQpdxJQbEj1ZdA0Ju2n7avJKJzf7OmdPPMt/P+UbQ8LlbX8tFV2Qdpj9C0rhX1JsgaG4Tyb5N+6znmRIdaVmlR8eATd0em6hIY="}'
+        const backupPrivateKey = '{"iv":"jPBIE/1S+NGckHMXVHDgcA==","v":1,"iter":10000,"ks":256,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"dV7sfCPyg3k=","ct":"2gJDm3OWenKfDfhVwvs3yLDk3IlwcDjUlnfdgCZ3bDs3Kv5h6BOgcEE5bLURwCQpdxJQbEj1ZdA0Ju2n7avJKJzf7OmdPPMt/P+UbQ8LlbX8tFV2Qdpj9C0rhX1JsgaG4Tyb5N+6znmRIdaVmlR8eATd0em6hIY="}'
+        const servicePublicKey = 'xpub661MyMwAqRbcG6wkoNfhLXdWQ8UnuB4K4mBhaT6WhW4Mnp7UCDo3CtiQWQzzXJMgPk1cjUqoff9TR9WN61tqmumzAq4DwuGx9xvuxEihXEA'
+        const encryptedPassword = '{"iv":"EVleTSjy2pGoU6q6+z7GJg==","v":1,"iter":10000,"ks":256,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"Ih4pF9Xq+kY=","ct":"JUsn+2+5vogUfCWx0cJXZ21/99+vgAOeY/LEg0jC+vaHmHvq/K4bdYfy"}'
+        const base64 = await generateBackupPdfBase64(userPrivateKey, backupPrivateKey, servicePublicKey, encryptedPassword)
+        const output = new Buffer(base64, 'base64')
+        fs.writeFileSync("/home/pb/Devel/sakiewka/testb64.pdf", output)
+    })
+
+    it('should generate base64 representation of pdf', async () => {
+        const userPrivateKey = '{"iv":"jPBIE/1S+NGckHMXVHDgcA==","v":1,"iter":10000,"ks":256,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"dV7sfCPyg3k=","ct":"2gJDm3OWenKfDfhVwvs3yLDk3IlwcDjUlnfdgCZ3bDs3Kv5h6BOgcEE5bLURwCQpdxJQbEj1ZdA0Ju2n7avJKJzf7OmdPPMt/P+UbQ8LlbX8tFV2Qdpj9C0rhX1JsgaG4Tyb5N+6znmRIdaVmlR8eATd0em6hIY="}'
+        const backupPrivateKey = '{"iv":"jPBIE/1S+NGckHMXVHDgcA==","v":1,"iter":10000,"ks":256,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"dV7sfCPyg3k=","ct":"2gJDm3OWenKfDfhVwvs3yLDk3IlwcDjUlnfdgCZ3bDs3Kv5h6BOgcEE5bLURwCQpdxJQbEj1ZdA0Ju2n7avJKJzf7OmdPPMt/P+UbQ8LlbX8tFV2Qdpj9C0rhX1JsgaG4Tyb5N+6znmRIdaVmlR8eATd0em6hIY="}'
+        const servicePublicKey = 'xpub661MyMwAqRbcG6wkoNfhLXdWQ8UnuB4K4mBhaT6WhW4Mnp7UCDo3CtiQWQzzXJMgPk1cjUqoff9TR9WN61tqmumzAq4DwuGx9xvuxEihXEA'
+        const encryptedPassword = '{"iv":"EVleTSjy2pGoU6q6+z7GJg==","v":1,"iter":10000,"ks":256,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"Ih4pF9Xq+kY=","ct":"JUsn+2+5vogUfCWx0cJXZ21/99+vgAOeY/LEg0jC+vaHmHvq/K4bdYfy"}'
+        const output = await generateBackupPdfBase64(userPrivateKey, backupPrivateKey, servicePublicKey, encryptedPassword)
+        expect(output).to.equal('generated base64 representation is different each time, probably due to some date changes, need to investigate')
     })
 })
