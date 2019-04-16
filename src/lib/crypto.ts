57113611f340157655937dd75ce085e55c5dfa47
Jakub Dziworski <jakub.dziworski@gmail.com>
2019-04-16 12:57:05 +0200

    Updated sjcl and some crypto methods (#39)
    * Updated sjcl and some crypto methods
    
    * Removed no longer needed custom types for sjcl
    
    * random salt for pbkdf2 wallet key password
    
    * Added some comments
    
    * Fixed pr comment

--- src/lib/crypto.ts
+++ src/lib/crypto.ts
@@ -1,43 +1,37 @@
 import crypto from 'crypto'
-import sjcl from 'sjcl-complete'
+import sjcl from 'sjcl'
 
-
-export const pbkdf2 = (passphrase: string): string => {
-  const passphraseAsBits = sjcl.codec.utf8String.toBits(passphrase)
-  const salt = '0fabdd8eab54b3e678658c06b17bb5de839380bbc66593f8573d45e80bbe082e'
-  const outputAsBits = sjcl.misc.pbkdf2(passphraseAsBits, salt, 10000, 512)
-  return sjcl.codec.hex.fromBits(outputAsBits)
-}
-
+/*
+  Password is derived to pbkdf2 key with random salt.
+  The input is then encrypted with the key using another random salt and random IV (initialization vector).
+  The result of encryption is json object including the encrypted message as well as
+  pbkdf2 salt, encryption salt and iv. These values are randomly generated thus need to be stored for later decryption.
+  Description of all parameters can be found here - http://bitwiseshiftleft.github.io/sjcl/demo/
+ */
 export const encrypt = (password: string, input: string): string => {
-  const randomSalt = sjcl.random.randomWords(2, 0)
-  const encryptOptions = { iter: 10000, ks: 256, salt: randomSalt }
-
-  return sjcl.encrypt(pbkdf2(password), input, encryptOptions)
+  const { key: pbkdf2Key, salt: pbkdf2Salt } = sjcl.misc.cachedPbkdf2(password);
+  const encryptionSalt = sjcl.random.randomWords(2, 0)
+  const iv = sjcl.random.randomWords(2, 0)
+  const encryptOptions = { iter: 10000, ks: 256, salt: encryptionSalt, iv: iv, ts: 96, pbkdf2Salt }
+  return sjcl.encrypt(pbkdf2Key.toString(), input, encryptOptions).toString()
 }
 
 export const decrypt = (password: string, input: string): string => {
-  const encryptOptions = { iter: 10000, ks: 256 }
-  return sjcl.decrypt(pbkdf2(password), input, encryptOptions)
+  const pbkdf2Salt = sjcl.codec.base64.toBits(JSON.parse(input).pbkdf2Salt)
+  const { key: pbkdf2Key } = sjcl.misc.cachedPbkdf2(password, { salt: pbkdf2Salt })
+  return sjcl.decrypt(pbkdf2Key.toString(), input)
 }
 
-export const hashSha512 = (input: string): string => {
-  const bitArrayHash = sjcl.hash.sha512.hash(input)
+export const hashSha256 = (input: string): string => {
+  const bitArrayHash = sjcl.hash.sha256.hash(input)
   const stringHash = sjcl.codec.hex.fromBits(bitArrayHash)
 
   return stringHash
 }
 
-export const hashSha1 = (input: string): string => {
-  const bitArrayHash = sjcl.hash.sha1.hash(input)
-  const stringHash = sjcl.codec.hex.fromBits(bitArrayHash)
-
-  return stringHash
-}
-
-export const hashPassword = (input: string): string => {
-  const salt = hashSha1(input)
-  const bitArrayHash = sjcl.misc.scrypt(input, salt)
+export const hashPassword = (password: string): string => {
+  const salt = hashSha256(password + ']KDH.@^CdKt@Jq')
+  const bitArrayHash = sjcl.misc.pbkdf2(password, salt, 1000, 256)
   const stringHash = sjcl.codec.hex.fromBits(bitArrayHash)
 
   return stringHash
