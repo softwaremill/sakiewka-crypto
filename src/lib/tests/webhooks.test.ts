aa5bcd2daf4d6b5f171533cef86203154f6a6e71
Dariusz Mydlarz <dmydlarz@gmail.com>
2019-04-18 08:54:28 +0200

    Add spec for createWebook function

--- src/lib/tests/webhooks.test.ts
+++ src/lib/tests/webhooks.test.ts
@@ -18,4 +18,25 @@ describe('createWebhook', () => {
   it('should exist', () => {
     expect(webhooksApi.createWebhook).to.be.a('function')
   })
+
+  it('should pass proper arguments to backend-api method and return result of its call', async () => {
+    // @ts-ignore
+    const mockImplementation = jest.fn(() => ({}))
+    // @ts-ignore
+    backendApi.createWebhook = mockImplementation
+
+    const res = await webhooksApi.createWebhook(
+      'testToken',
+      'testWalletId',
+      'http://test.callback.com',
+      {}
+    )
+
+    const [token, walletId, callbackUrl, settings] = mockImplementation.mock.calls[0]
+    expect(token).to.eq('testToken')
+    expect(walletId).to.eq('testWalletId')
+    expect(callbackUrl).to.eq('http://test.callback.com')
+    expect(settings).to.be.a('object').that.is.empty
+    expect(res).to.be.a('object').that.is.empty
+  })
 })
