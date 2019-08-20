79c445c4144280e589084440f86d4772c4c78f49
ghostbuster91 <kghost0@gmail.com>
2019-08-20 15:14:40 +0200

    [eos] Add transfer method

---
+++ src/lib/tests/eos/eos-transaction.test.ts
@@ -0,0 +1,35 @@
+// import { expect } from 'chai'
+// import ecc from 'eosjs-ecc'
+// import { Api, JsonRpc } from 'eosjs'
+
+// describe('eos account', () => {
+//   it('should create offline signed newaccount transaction', async () => {
+//     const transferToken = {
+//       expiration: '2019-08-19T14:01:26',
+//       ref_block_num: 16,
+//       ref_block_prefix: 3257616527,
+//       max_net_usage_words: 0,
+//       max_cpu_usage_ms: 0,
+//       delay_sec: 0,
+//       context_free_actions: [],
+//       actions: [
+//         {
+//           account: 'eosio.token',
+//           name: 'transfer',
+//           authorization: [
+//             {
+//               actor: 'alice',
+//               permission: 'active',
+//             },
+//           ],
+//           data:
+//             '0000000000855c340000000000000e3d90d0030000000000044b415300000000104865726520796f7520676f20626f6221',
+//         },
+//       ],
+//       transaction_extensions: [],
+//       signatures: [],
+//       context_free_data: [],
+//     }
+//     expect(false).to.be.eq(true)
+//   })
+// })
