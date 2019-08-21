efabc131d0f953c7944d01a5d40d9c0fbb354370
ghostbuster91 <kghost0@gmail.com>
2019-08-21 11:05:41 +0200

    [Eos] Simplify eosAPi object creation

--- src/lib/eos/eos-transaction.ts
+++ src/lib/eos/eos-transaction.ts
@@ -36,7 +36,7 @@ export const eosTransactionModuleFactory = (
       quantity: string,
       now?: moment.Moment,
     ) =>
-      packedTransfer(
+      packedTransfer2(
         chainId,
         privateKeys,
         refBlockNumber,
@@ -89,6 +89,60 @@ const packedTransfer = async (
     rpc,
     signatureProvider,
     authorityProvider,
+    abiProvider,
+    chainId,
+    textDecoder: new TextDecoder(),
+    textEncoder: new TextEncoder(),
+  })
+  const t = transfer(
+    expiration,
+    refBlockNumber,
+    refBlockPrefix,
+    from,
+    to,
+    quantity,
+  )
+  const response = await api.transact(t, {
+    broadcast: false,
+    sign: false,
+  })
+  return {
+    serializedTransaction: Buffer.from(response.serializedTransaction).toString(
+      'hex',
+    ),
+  }
+}
+
+const packedTransfer2 = async (
+  chainId: string,
+  privateKeys: string[],
+  refBlockNumber: number,
+  refBlockPrefix: number,
+  from: string,
+  to: string,
+  quantity: string,
+  now?: moment.Moment,
+) => {
+  const abiProvider = {
+    getRawAbi: (accountName: string): Promise<BinaryAbi> => {
+      // This base64 encoded eosio abi with system contract installed (required for buyrambytes and delegatebw actions) - https://github.com/EOS-Mainnet/governance/tree/master/eosio.system
+      // Once you install the contract you can get the base64 version by calling curl --request POST --data '{"account_name":"eosio"}' --url http://127.0.0.1:8888/v1/chain/get_raw_abi
+      const base64Abi =
+        'DmVvc2lvOjphYmkvMS4xAAgHYWNjb3VudAABB2JhbGFuY2UFYXNzZXQFY2xvc2UAAgVvd25lcgRuYW1lBnN5bWJvbAZzeW1ib2wGY3JlYXRlAAIGaXNzdWVyBG5hbWUObWF4aW11bV9zdXBwbHkFYXNzZXQOY3VycmVuY3lfc3RhdHMAAwZzdXBwbHkFYXNzZXQKbWF4X3N1cHBseQVhc3NldAZpc3N1ZXIEbmFtZQVpc3N1ZQADAnRvBG5hbWUIcXVhbnRpdHkFYXNzZXQEbWVtbwZzdHJpbmcEb3BlbgADBW93bmVyBG5hbWUGc3ltYm9sBnN5bWJvbAlyYW1fcGF5ZXIEbmFtZQZyZXRpcmUAAghxdWFudGl0eQVhc3NldARtZW1vBnN0cmluZwh0cmFuc2ZlcgAEBGZyb20EbmFtZQJ0bwRuYW1lCHF1YW50aXR5BWFzc2V0BG1lbW8Gc3RyaW5nBgAAAAAAhWlEBWNsb3NlAAAAAACobNRFBmNyZWF0ZQAAAAAAAKUxdgVpc3N1ZQAAAAAAADBVpQRvcGVuAAAAAACo67K6BnJldGlyZQAAAABXLTzNzQh0cmFuc2ZlcgACAAAAOE9NETIDaTY0AAAHYWNjb3VudAAAAAAAkE3GA2k2NAAADmN1cnJlbmN5X3N0YXRzAAAAAA==='
+      return Promise.resolve({ abi: base64ToBinary(base64Abi), accountName })
+    },
+  }
+  const expiration = (now || moment())
+    .add(30, 'minutes')
+    .toDate()
+    .toString()
+  const api = new Api({
+    // @ts-ignore
+    rpc: null,
+    // @ts-ignore
+    signatureProvider: null,
+    // @ts-ignore
+    authorityProvider: null,
     abiProvider,
     chainId,
     textDecoder: new TextDecoder(),
