c8562e0bbe3ed0840fb9564a8e80f551569c5b1f
ghostbuster91 <kghost0@gmail.com>
2019-08-23 15:38:59 +0200

    Fix incorrect btc/btg networks

--- src/lib/network.ts
+++ src/lib/network.ts
@@ -3,8 +3,8 @@ export type ChainNetwork = { [C in Currency]: string }
 
 export const networks = (eosChainId?: string) => ({
   testnet: {
-    [Currency.BTC]: 'testnet',
-    [Currency.BTG]: 'testnet',
+    [Currency.BTC]: 'mainnet',
+    [Currency.BTG]: 'mainnet',
     [Currency.EOS]:
       'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
   } as ChainNetwork,
@@ -15,8 +15,8 @@ export const networks = (eosChainId?: string) => ({
       'e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473', // jungle testnet v2
   } as ChainNetwork,
   regtest: {
-    [Currency.BTC]: 'testnet',
-    [Currency.BTG]: 'testnet',
+    [Currency.BTC]: 'regtest',
+    [Currency.BTG]: 'regtest',
     [Currency.EOS]: eosChainId || 'NOT SET',
   } as ChainNetwork,
 })
