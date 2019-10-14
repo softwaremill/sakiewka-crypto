a485c0d0cecb18480ddd31fd7cd3a400670c6578
ghostbuster91 <kghost0@gmail.com>
2019-10-14 18:18:57 +0200

    Fix deprecation warning

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   globals: {
-    "ts-jest": {
-      tsConfig: "tsconfig.json"
-    }
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
+    },
   },
-  moduleFileExtensions: ["js", "ts"],
+  moduleFileExtensions: ['js', 'ts'],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
-  testMatch: ["**/src/**/*.test.(ts|js)"],
-  testEnvironment: "node",
-  setupTestFrameworkScriptFile: "./jest.setup.js",
-  preset: "ts-jest"
-};
+  testMatch: ['**/src/**/*.test.(ts|js)'],
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  preset: 'ts-jest',
+}
